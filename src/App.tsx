ec2954b1e31ac30b36f066f3b215d144d990a044
nrevs <nicholaswagner@revature.net>
2021-05-03 22:35:22 -0400

    resolving merge conflicts src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import logo from './logo.svg';
 
 import React, { Component } from 'react';
@@ -6,7 +6,6 @@ import StudyApp from './components/flashcards/StudyApp';
 
 import './App.css';
 
-<<<<<<< HEAD:src/App.js
 import './bootstrap.css'
 
 
@@ -20,27 +19,7 @@ class App extends Component {
       </div>
     );
   }
-=======
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 6142b54dc7c4e31f79372269e1e01a9a38beffd6:src/App.tsx
+
 }
 
 // function App() {
